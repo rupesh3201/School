f82eea72649ec82c550cd19ae0c9b6ba25dd7fda
rupesh3201 <rupesh.mandge3201@gmail.com>
2025-02-15 11:55:59 +0530

    Server set up done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // setup done
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+const Port = 5003 ;
+app.listen(Port, ()=>
+{
+    console.log(`server is running on https//localhost:${Port}`);
+
+});
